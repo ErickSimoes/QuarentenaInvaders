86f57d76cf4734cdf85853808496b30e0489ce33
Erick Simões <erick.simoes@live.com>
2020-05-09 23:34:18 -0300

    Add Dispatcher on project

---
+++ Assets/Battlehub/Dispatcher/DispatcherTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Threading;
+
+namespace Battlehub.Dispatcher
+{
+    public class DispatcherTest : MonoBehaviour
+    {
+        [SerializeField]
+        private Text Output;
+
+        private void Start()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread t = new Thread(ThreadFunction);
+                t.Start("Dispatched from Thread " + i);
+            }
+        }
+        
+        private void ThreadFunction(object param)
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                Output.text += param;
+                Output.text += Environment.NewLine;
+            });
+        }
+    }
+}
